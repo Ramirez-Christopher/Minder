beebed09d427daac89e5395f25579d25202eee85
Ramirez-Christopher <chris.ramirez@nevada.unr.edu>
2024-07-13 14:14:01 -0700

    writing protected routes

---
+++ frontend/src/components/protectedRoute.jsx
@@ -0,0 +1,62 @@
+import {navigate} from '@reach/router';
+import {jwtDecode} from 'jwt-decode';
+import api from '../api';
+import { REFRESH, ACCESS_TOKEN } from '../constants';
+import { useState, useEffect } from 'react';
+
+function ProtectedRoute({children}) {
+    const [isAuthorized, setIsAuthorized] = useState(null);
+
+    useEffect(() => {
+        auth().catch(() => {setIsAuthorized(false) });
+
+    }, [])
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH);
+        try{
+            const res = await api.post("/api/token/refresh/", {
+            refresh: refreshToken});
+
+            if(res.status == 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+            
+
+        } catch {
+            console.log(error);
+            setIsAuthorized(false);
+
+        }
+    };
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if(!token) {
+            setIsAuthorized(false);
+            return;
+
+        }
+        const decoded = jwtDecode(token);
+        const TokenExpiration = decoded.exp
+        const now = Date.now() / 1000; //date in seconds not in ms
+
+        if(TokenExpiration < now) {
+            await refreshToken();
+        } else {
+            setIsAuthorized(true);
+            
+        }
+
+        if (isAuthorized == null) {
+            return <div>Loading...</div>
+        }
+    
+    }
+
+    return isAuthorized ? children : <navigate to="/login" />
+
+}
